2f43c0758532259466f4dcfdd9a7fce3b2697efd
Wei Lu <wadelu0714@gmail.com>
2020-01-15 23:28:31 +1300

    Connnect to the database

---
+++ config/db.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const config = require('config')
+
+const db = config.get('mongoURI')
+
+const connectDB = async () => {
+
+  //The async/await version
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.error(error.message)
+    process.exit(1)
+  }
+
+  //The promise version
+  //I also need to get rid of the "async" keyword before the arrow function
+
+  // mongoose.connect(db, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true
+  // })
+  //   .then(() => console.log('MongoDB Connected'))
+  //   .catch(err => {
+  //     console.error(err.message)
+  //     process.exit(1)
+  //   })
+}
+
+module.exports = connectDB
