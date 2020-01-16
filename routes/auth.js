010aecbe63bf14153f8b31b39d03d7072feb6919
Wei Lu <wadelu0714@gmail.com>
2020-01-16 17:47:19 +1300

    add user log in api

--- routes/auth.js
+++ routes/auth.js
@@ -6,14 +6,21 @@ const { check, validationResult } = require('express-validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const config = require('config')
+const auth = require('../middleware/auth')
 //User is a class
 const User = require('../models/User')
 
 // @route      GET api/auth
 // @desc       Get logged in user
 // @access     Private
-router.get('/', (req, res) => {
-  res.send('Get logged in user')
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+    res.json(user)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send('Server Error')
+  }
 })
 
 // @route      POST api/auth
