d425362f529793b79e7fb26eb450f98c34edb89b
Wei Lu <wadelu0714@gmail.com>
2020-01-17 14:55:48 +1300

    function: add a product to the database

---
+++ models/Product.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = mongoose.Schema({
+  product_id: {
+    //0101001
+    //AABBCCC
+    //AA category
+    //BB subcategory
+    //CCC product
+    type: String,
+    required: true,
+    unique: true
+  },
+  name_eng: {
+    type: String,
+    default: 'no eng name'
+  },
+  name_chn: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  subcategory: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    default: 'no description'
+  },
+  image: {
+    type: String
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+})
+
+//a mongoose model called 'user', it is a class
+module.exports = mongoose.model('product', ProductSchema)
--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,8 @@
 const express = require('express')
-
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
 
+const Product = require('../models/Product')
 // @route      GET api/products
 // @desc       Get all users products
 // @access     Private
@@ -9,6 +10,53 @@ router.get('/', (req, res) => {
   res.send('Get all products')
 })
 
-//and the other route
+// @route       POST api/products
+// @desc        Post an product into the mongoDB
+// @access      For now, it is public, at last, only the admin user can do that
+
+router.post('/', [
+  check('product_id', 'A product id is required').not().isEmpty(),
+  check('name_chn', 'A product name in Chinese is required').not().isEmpty(),
+  check('category', 'The category of the product is required').not().isEmpty(),
+  check('subcategory', 'The subcategory of the product is required').not().isEmpty(),
+  check('price', 'The price of the product in Chinese Yuan is required').not().isEmpty()
+], async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array()
+    })
+  }
+
+  const { product_id, name_eng, name_chn, category, subcategory, description, image, price } = req.body
+
+
+  try {
+    //check if the product id exists
+    let product = await Product.findOne({ product_id })
+    if (product) {
+      return res.status(400).json({
+        msg: 'Product already exists'
+      })
+    }
+
+    //save it into the database
+    product = new Product({
+      product_id,
+      name_eng,
+      name_chn,
+      category,
+      subcategory,
+      description,
+      image,
+      price
+    })
+    await product.save()
+    res.send(product)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send('Server Error')
+  }
+})
 
 module.exports = router
